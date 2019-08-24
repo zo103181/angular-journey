10500266bea72710fd9adf0f07812c56740fcace
Zach Olson <zo103181@gmail.com>
2019-08-24 07:03:37 -0500

    add providers to our app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// Providers
+import { ErrorsHandler } from './shared/providers/error-handler';
+import { HttpsInterceptor } from './shared/providers/http-interceptor';
 
 /* 
   Used to verify we have things setup properly.
   Will be moving to a SharedModule later 
 */
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,10 +26,14 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatSnackBarModule,
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorHandler, useClass: ErrorsHandler },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpsInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
