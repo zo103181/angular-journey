a5495896b258239ea9edfdf5441f6c0fa83dad4f
Zach Olson <zo103181@gmail.com>
2019-08-24 07:02:44 -0500

    generate error-handler & http-interceptor

---
+++ src/app/shared/providers/error-handler.ts
@@ -0,0 +1,35 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+
+// Services
+import { NotificationService } from '../services/notification.service';
+import { LoggingService } from '../services/logging.service';
+
+@Injectable()
+export class ErrorsHandler implements ErrorHandler {
+
+    // Error handling is important and needs to be loaded first.
+    // Because of this we should manually inject the services with Injector.
+    constructor(private injector: Injector) { }
+
+    handleError(error: Error | HttpErrorResponse) {
+
+        const notifier = this.injector.get(NotificationService);
+        const logger = this.injector.get(LoggingService);
+
+        if (!navigator.onLine) {
+            // Handle offline error
+            notifier.showError('Browser Offline!');
+        } else {
+            if (error instanceof HttpErrorResponse) {
+                // Handle Http Error (4xx, 5xx, ect.)
+                notifier.showError('Http Error: ' + error.message);
+            } else {
+                // Handle Client Error (Angular Error, ReferenceError...)
+                notifier.showError(error.message);
+            }
+            // Always log the error
+            logger.logError(error);
+        }
+    }
+}
---
+++ src/app/shared/providers/http-interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent, HttpRequest, HttpHandler,
+  HttpInterceptor, HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
+@Injectable()
+export class HttpsInterceptor implements HttpInterceptor {
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    return next.handle(request).pipe(
+      retry(1),
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          // Attempt to refresh the current token, however if that fails redirect to login
+          return throwError(error);
+        } else {
+          return throwError(error);
+        }
+      })
+    );
+  }
+}
