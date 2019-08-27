724dde2701c35c0cdb723881fdae974f2fcf3d52
Zach Olson <dev@sell-on.com>
2019-08-27 09:24:35 -0500

    add credentials to project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBG5nP3WdbLtO8jQjlwIDB3rbEXtm_8k-I',
+    authDomain: 'angular7-journey.firebaseapp.com',
+    databaseURL: 'https://angular7-journey.firebaseio.com',
+    projectId: 'angular7-journey',
+    storageBucket: '',
+    messagingSenderId: '587987135634',
+    appId: '1:587987135634:web:b0d95754b804d851'
+  }
 };
 
 /*
