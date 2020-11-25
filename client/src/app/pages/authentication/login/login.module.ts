38e081310a882ab44baaa1a9a1435089f7bc3c31
Zach Olson <zo103181@gmail.com>
2020-11-25 06:30:46 -0600

    npm nodemon/express & setup client/server

--- client/src/app/pages/authentication/login/login.module.ts
+++ client/src/app/pages/authentication/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { SharedModule } from 'src/client/app/shared/shared.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { LoginComponent } from './login.component';
 
 const routes = [{
---
+++ server/www.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const app = express();
+const path = require('path');
+
+app.use(bodyParser.json());
+
+// app.use(express.static(__dirname + '../../../dist'));
+
+app.get('/api/status', (req, res) => {
+  res.json({info: 'Node.js, Express, and Postgres API'});
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
+});
+
+// Listen to the specified port, otherwise 3080
+const PORT = process.env.PORT || 3080;
+const server = app.listen(PORT, () => {
+  console.log(`Server Running: http://localhost:${PORT}`);
+});
+
+/**
+ * The SIGTERM signal is a generic signal used to cause program 
+ * termination. Unlike SIGKILL , this signal can be blocked, 
+ * handled, and ignored. It is the normal way to politely ask a 
+ * program to terminate. The shell command kill generates 
+ * SIGTERM by default.
+ */
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Server Close: Process Terminated!');
+    });
+});
