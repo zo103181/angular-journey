700af8f662caebe0b8ab05c62ffea1ebbe767d90
Zach Olson <zo103181@gmail.com>
2022-03-23 13:16:42 -0500

    add basic auth guard

--- client/src/app/pages/settings/settings.routing.ts
+++ client/src/app/pages/settings/settings.routing.ts
@@ -1,9 +1,11 @@
 import { Route } from '@angular/router';
 import { SettingsComponent } from './settings.component';
+import { AuthGuard } from '../../core/auth/guards/auth.guard';
 
 export const settingsRoutes: Route[] = [
     {
         path: '',
-        component: SettingsComponent
+        component: SettingsComponent,
+        canActivate: [AuthGuard]
     }
 ];
